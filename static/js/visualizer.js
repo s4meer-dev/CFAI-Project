9e8b7dd52322818eca9cf16a3e67713e106aab24
Savithru <savithrudhulipala455@gmail.com>
2026-06-07 22:03:15 +0530

    feat(js): implement bubble sort step generator in visualizer

--- static/js/visualizer.js
+++ static/js/visualizer.js
@@ -50,6 +50,21 @@ class Visualizer {
         );
         this.draw();
     }
+
+    *bubbleSortSteps(arr) {
+        const n = arr.length;
+        for (let i = 0; i < n - 1; i++) {
+            for (let j = 0; j < n - i - 1; j++) {
+                yield { indices: [j, j + 1], action: 'compare' };
+                if (arr[j] > arr[j + 1]) {
+                    yield { indices: [j, j + 1], action: 'swap' };
+                    [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                }
+            }
+            this.sortedIndices.add(n - 1 - i);
+        }
+        this.sortedIndices.add(0);
+    }
 }
 
 const visualizer = new Visualizer('sortCanvas');
